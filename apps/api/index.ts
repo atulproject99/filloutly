5cc91bec7c3899012d3e952bde82cee3d4d963a1
atul <atulmaurya773@gmail.com>
2026-05-27 22:20:03 +0530

    fix: uncomment server initialization code and create new entry point

---
+++ apps/api/index.ts
@@ -0,0 +1,3 @@
+import app from "../api/src/server";
+
+export default app;
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,24 +1,20 @@
-// import http from "node:http";
-// import { logger } from "@repo/logger";
-// import { app as expressApplication } from "./server";
+import { logger } from "@repo/logger";
+import http from "node:http";
+import { app as expressApplication } from "./server";
 
-// import { env } from "./env";
+import { env } from "./env";
 
-// async function init() {
-//   try {
-//     const server = http.createServer(expressApplication);
-//     const PORT: number = env.PORT ? +env.PORT : 8000;
-//     server.listen(PORT, () => {
-//       logger.info(`http server is running on PORT ${PORT}`);
-//     });
-//   } catch (err) {
-//     logger.error(`Error creating http server`, { err });
-//     process.exit(1);
-//   }
-// }
+async function init() {
+  try {
+    const server = http.createServer(expressApplication);
+    const PORT: number = env.PORT ? +env.PORT : 8000;
+    server.listen(PORT, () => {
+      logger.info(`http server is running on PORT ${PORT}`);
+    });
+  } catch (err) {
+    logger.error(`Error creating http server`, { err });
+    process.exit(1);
+  }
+}
 
-// init();
-
-import app from "../src/server";
-
-export default app;
+init();
